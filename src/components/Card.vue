269d05add0d7eee59838e248693ecf624221a0ab
vitodursopettersson <vitodurso98@gmail.com>
2023-12-19 00:11:40 +0100

    ratings star

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -20,7 +20,30 @@ export default {
         <h6 v-if="lang === 'en'">🇬🇧 {{ lang }}</h6>
         <h6 v-else-if="lang === 'it'">🇮🇹 {{ lang }}</h6>
         <h6 v-else>{{ lang }}</h6>
-        <h6>{{ ratings }}</h6>
+        <div>
+            <h6 v-if="Math.floor(ratings) === 10 || Math.floor(ratings) === 9">
+                <font-awesome-icon icon="fa-solid fa-star" v-for="n in 5" />
+            </h6>
+            <h6 v-else-if="Math.floor(ratings) === 8 || Math.floor(ratings) === 7">
+                <font-awesome-icon icon="fa-solid fa-star" v-for="n in 4" />
+                <font-awesome-icon icon="fa-regular fa-star" />
+            </h6>
+            <h6 v-else-if="Math.floor(ratings) === 6 || Math.floor(ratings) === 5">
+                <font-awesome-icon icon="fa-solid fa-star" v-for="n in 3" />
+                <font-awesome-icon icon="fa-regular fa-star" v-for="n in 2" />
+            </h6>
+            <h6 v-else-if="Math.floor(ratings) === 4 || Math.floor(ratings) === 3">
+                <font-awesome-icon icon="fa-solid fa-star" v-for="n in 2" />
+                <font-awesome-icon icon="fa-regular fa-star" v-for="n in 3" />
+            </h6>
+            <h6 v-else-if="Math.floor(ratings) === 2 || Math.floor(ratings) === 1">
+                <font-awesome-icon icon="fa-solid fa-star" />
+                <font-awesome-icon icon="fa-regular fa-star" v-for="n in 4" />
+            </h6>
+            <h6 v-else>
+                <font-awesome-icon icon="fa-regular fa-star" v-for="n in 5" />
+            </h6>
+        </div>
     </div>
 </template>
 
