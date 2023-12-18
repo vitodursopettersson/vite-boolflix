5b8dc20df432a4241a2aaeeb5124dcf7a64e3092
vitodursopettersson <vitodurso98@gmail.com>
2023-12-18 23:52:24 +0100

    new dependencies fontawesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
+// Vue Js
 import { createApp } from 'vue'
 import './assets/styles/general.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
+library.add(faStar, faStarEmpty)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
+
+
